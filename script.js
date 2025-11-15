977ba2e11fcdcd8ae473779c21334f67f7d4ce3e
GospelExcel <gexcel123@gmail.com>
2025-11-14 19:18:12 -0500

    Make dummy UI

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+let num1;
+let num2;
+let operation;
+
+let keypad = document.getElementById('keypad')
+
+for(let i = 0; i < 10; i++) {
+    const newButton = document.createElement('button')
+    newButton.textContent = `${i}`
+    newButton.classList.add('keypad_digit')
+    keypad.appendChild(newButton)
+}
+
 function add (num1, num2) {
     return num1 + num2;
 }
@@ -12,4 +25,8 @@ function multiply (num1, num2) {
 
 function divide (num1, num2) {
     return num1 / num2;
+}
+
+function operate (operator, num1, num2) {
+    return add(num1, num2)
 }
