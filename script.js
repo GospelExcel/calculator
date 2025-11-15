c72cbdc82f6b55cd80a2bae180e2b5f74e496e7b
GospelExcel <gexcel123@gmail.com>
2025-11-15 10:45:42 -0500

    clean buttons

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ let num1;
 let num2;
 let operation;
 
-let keypad = document.getElementById('keypad')
+let buttons = document.getElementById('buttons')
 
 for(let i = 1; i < 10; i++) {
     const newButton = document.createElement('button');
     newButton.textContent = `${i}`;
-    newButton.classList.add('keypad_digit');
-    keypad.appendChild(newButton);
+    newButton.classList.add('calc_button');
+    buttons.appendChild(newButton);
 }
 
-const zButton = document.createElement('button');
-zButton.textContent = '0';
-zButton.classList.add('keypad_digit');
-keypad.appendChild(zButton);
+// const zButton = document.createElement('button');
+// zButton.textContent = '0';
+// zButton.classList.add('keypad_digit');
+// keypad.appendChild(zButton);
 
 function add (num1, num2) {
     return num1 + num2;
